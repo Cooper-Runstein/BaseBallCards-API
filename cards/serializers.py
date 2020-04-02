7568ae7abbbe268e493f536c98ef6a978ef2f5c6
Cooper Runstein <cooper.runstein@vacasa.com>
2020-04-01 22:28:40 -0700

    Increase depth for BaseBallCards serializer

--- cards/serializers.py
+++ cards/serializers.py
@@ -1,7 +1,7 @@
 from django.contrib.auth.models import User, Group
 from rest_framework import serializers
 
-from cards.models import BaseBallCard
+from cards.models import BaseBallCard, Player
 
 
 class UserSerializer(serializers.HyperlinkedModelSerializer):
@@ -21,3 +21,10 @@ class BaseBallCardSerializer(serializers.ModelSerializer):
     class Meta:
         model = BaseBallCard
         fields = ['year', "player", "team", "company"]
+        depth = 1
+
+
+class PlayerSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Player
+        fields = ["first_name", "last_name"]
